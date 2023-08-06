004f79d72a1245270d3f3e61e3da5b7a900e3735
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2023-08-06 10:27:58 +0700

    Pemahaman bagaimana Activity bekerja : Aplikasi Hitung Sederhana

--- app/src/main/java/com/oasis/tino/MainActivity.kt
+++ app/src/main/java/com/oasis/tino/MainActivity.kt
@@ -1,57 +1,58 @@
 package com.oasis.tino
 
 import android.os.Bundle
+import android.view.View
 
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-
-import android.view.Menu
-import android.view.MenuItem
-
-import com.oasis.tino.databinding.ActivityMainBinding
-
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var panjang: EditText
+    private lateinit var lebar: EditText
+    private lateinit var tinggi: EditText
+    private lateinit var buttonHitung: Button
+    private lateinit var hasil: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        panjang =   findViewById(R.id.panjang)
+        lebar   =   findViewById(R.id.lebar)
+        tinggi  =   findViewById(R.id.tinggi)
+        buttonHitung    =   findViewById(R.id.hitung)
+        hasil   =   findViewById(R.id.hasil)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        buttonHitung.setOnClickListener(this)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.hitung){
+            val panjangValue    =   panjang.text.toString().trim()
+            val lebarValue      =   lebar.text.toString().trim()
+            val tinggiValue     =   tinggi.text.toString().trim()
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+            var fieldEmpty  =   false
+            if(panjangValue.isEmpty()){
+                fieldEmpty      =   true
+                panjang.error   =   "Panjang tidak boleh kosong!"
+            }
+            if(lebarValue.isEmpty()){
+                fieldEmpty  =   true
+                lebar.error =   "Lebar tidak boleh kosong"
+            }
+            if(tinggiValue.isEmpty()){
+                fieldEmpty      =   true
+                tinggi.error    =   "Tinggi tidak boleh kosong"
+            }
+
+            if(!fieldEmpty) {
+                val volume =
+                    panjangValue.toDouble() * lebarValue.toDouble() * tinggiValue.toDouble()
+                hasil.text = volume.toString()
+            }
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
